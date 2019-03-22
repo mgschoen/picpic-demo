2053ba0cb53f1b699798b01e276c903112dd5526
Martin Schoen <schoenm@cip.ifi.lmu.de>
2019-03-22 18:00:21 +0100

    Add Promise polyfill

--- src/js/polyfill.js
+++ src/js/polyfill.js
@@ -1,5 +1,8 @@
 function attachPolyfills () {
 
+    // Promises
+    require('es6-promise')
+
     // IE11 NodeList.forEach()
     if (window.NodeList && !NodeList.prototype.forEach) {
         NodeList.prototype.forEach = Array.prototype.forEach
